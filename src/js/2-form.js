59c61452b1dff704cebac463b42661ae6cf1d33c
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2024-05-05 11:33:41 +0200

    finish

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,8 +1,42 @@
-const form = document.querySelector(".feedback-form");
-const inputs = form.querySelector("input, textarea");
+let formDate = {
+    email: "",
+    message: ""
+};
 
-inputs.forEach(input => {
-    input.addEventListener('input', (event) => {
-        localStorage.setItem(event.target.name, event.target.value);
-    });
-});
+const form = document.querySelector(".feedback-form");
+const inputEmail = form.querySelector('input[name="email"]');
+const inputMessage = form.querySelector('textarea[name="message"]');
+const buttonSubmit = form.querySelector('button[type="submit"]');
+
+const feedbackFormState = localStorage.getItem("feedback-form-state");
+if(feedbackFormState){
+    formDate = JSON.parse(feedbackFormState);
+    inputEmail.value = formDate.email;
+    inputMessage.value = formDate.message;
+};
+function saveToLocalStorage() {
+    localStorage.setItem("feedback-form-state", JSON.stringify(formDate));
+}
+
+form.addEventListener("input", (event) => {
+    const { name, value } = event.target;
+    formDate[name] = value;
+    saveToLocalStorage();
+})
+function clearFormDate() {
+    localStorage.removeItem("feedback-form-state");
+    formDate.email = "";
+    formDate.message = "";
+    inputEmail.value = "";
+    inputMessage.value = "";
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (formDate.email && formDate.message) {
+        console.log(formDate);
+        clearFormDate();
+    } else {
+        alert("Fill please all fields");
+    }
+})
